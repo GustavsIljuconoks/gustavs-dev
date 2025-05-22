f1a2eac6e2116b987e626f14799e33bf163efda4
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2025-05-22 23:15:57 +0300

    chore: update info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,47 +2,41 @@ import React from 'react';
 import ReactTyped from "react-typed";
 
 import '../css/Hero.css';
-import { logo } from '../assets';
 import { portrait } from '../assets';
 import { roles } from '../constants';
 
 const Hero = () => {
-  return (
-    <>
-      <section id='home' className='hero'>
-        <div className="container">
-          <div className="content mt-20">
-            <div className="personal flex justify-between items-center">
-               <div className="w-fit">
-                <img className='rounded-full lg:h-20 lg:w-20 md:h-16 md:w-16 w-12 h-12 hover:scale-[1.05] hover:shadow-custom1 hover:-rotate-12 transition-all duration-300 ease-in-out' src={portrait} alt="profile pic" />
-               </div>
+    return (
+        <>
+            <section id='home' className='hero'>
+                <div className="container">
+                    <div className="content mt-20">
+                        <div className="personal flex justify-between items-center">
+                            <div className="w-fit">
+                                <img className='rounded-full lg:h-20 lg:w-20 md:h-16 md:w-16 w-12 h-12 hover:scale-[1.05] hover:shadow-custom1 hover:-rotate-12 transition-all duration-300 ease-in-out' src={portrait} alt="profile pic" />
+                            </div>
+                        </div>
 
-              <div className="flex items-center justify-between space-x-2">
-                <div className='h-[1px] md:w-10 sm:w-7 w-4 bg-black'></div>
-                <a className='font-bold hover:text-accent transition-colors duration-300 ease-in-out capitalize md:text-base xl:text-lg text-xs' href='#contact'>Available For Work</a>
-              </div>
-            </div>
+                        <div className="hero-text mt-10">
+                            <h1 className='font-bold text-[60px] md:text-[80px]'>Hi, I'm <span className='text-accent'>Gustavs</span></h1>
 
-            <div className="hero-text mt-10">
-              <h1 className='font-bold text-[60px] md:text-[80px]'>Hi, I'm <span className='text-accent'>Gustavs</span></h1>
-
-              <h1 className='font-light text-[40px] md:text-[50px]'>
-                I'm a{" "}
-                <ReactTyped
-                  strings={roles}
-                  typeSpeed={100}
-                  loop
-                  backSpeed={80}
-                  cursorChar="|"
-                  showCursor={true}
-                />
-              </h1>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
+                            <h1 className='font-light text-[40px] md:text-[50px]'>
+                                I'm a{" "}
+                                <ReactTyped
+                                    strings={roles}
+                                    typeSpeed={100}
+                                    loop
+                                    backSpeed={80}
+                                    cursorChar="|"
+                                    showCursor={true}
+                                />
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Hero
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,149 +1,145 @@
 import {
-  javascript,
-  html,
-  css,
-  reactjs,
-  tailwind,
-  python,
-  figma,
-  forms,
-  kreics,
-  development,
-  threejs,
-  maui,
-  mysql,
-  django,
+    javascript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    python,
+    figma,
+    forms,
+    kreics,
+    development,
+    threejs,
+    maui,
+    mysql,
+    django,
 } from "../assets";
-  
+
 export const navLinks = [
-  {
-    link: "#about",
-    title: "About",
-  },
-  {
-    link: "#project",
-    title: "Projects",
-  },
-  {
-    link: "#contact",
-    title: "Contact",
-  },
+    {
+        link: "#about",
+        title: "About",
+    },
+    {
+        link: "#project",
+        title: "Projects",
+    },
+    {
+        link: "#contact",
+        title: "Contact",
+    },
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "Django",
-    icon: django
-  },
-  {
-    name: "MySQL",
-    icon: mysql,
-  },
-  {
-    name: "Python",
-    icon: python,
-  },
-  {
-    name: "MAUI",
-    icon: maui,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "Django",
+        icon: django
+    },
+    {
+        name: "MySQL",
+        icon: mysql,
+    },
+    {
+        name: "Python",
+        icon: python,
+    },
+    {
+        name: "MAUI",
+        icon: maui,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+
 ];
 
 const projects = [
-  {
-    name: "Lauku Forms",
-    description:
-      `Web-based form creator project designed and created for "Latvijas Lauku konsultāciju un izglītības centrs".`,
-    tags: [
-      {
-        name: "Django",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "MySQL",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Bootstrap",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: forms,
-    source_code_link: "https://github.com/27-Club/27club",
-  },
-  {
-    name: "kreics.com",
-    description:
-      `Portfolio website for filmaker "kreicsfilms". It's still in development.`,
-    tags: [
-      {
-        name: "React",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Tailwind",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Firebase",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: kreics,
-    source_code_link: "https://github.com/GustavsIljuconoks/kreics.com/",
-  },
-  {
-    name: "Battleships",
-    description:
-      `Popular Battleship board game online version. It's still in development.`,
-    tags: [
-      {
-        name: ".NET MAUI",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "C#",
-        color: "green-text-gradient",
-      }
-    ],
-    image: development,
-    source_code_link: "https://github.com/",
-  },
+    {
+        name: "Battleships",
+        description:
+            `Popular Battleship board game online version. It's still in development.`,
+        tags: [
+            {
+                name: "C#",
+                color: "green-text-gradient",
+            },
+            {
+                name: "TypeScript",
+                color: "blue-text-gradient",
+            }
+        ],
+        image: development,
+        source_code_link: "https://github.com/GustavsIljuconoks/pirate-adventure",
+    },
+    {
+        name: "Lauku Forms",
+        description:
+            `Web-based form creator project designed and created for "Latvijas Lauku konsultāciju un izglītības centrs".`,
+        tags: [
+            {
+                name: "Django",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "MySQL",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Bootstrap",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: forms,
+        source_code_link: "https://github.com/27-Club/27club",
+    },
+    {
+        name: "kreics.com",
+        description:
+            `Portfolio website for filmaker "kreicsfilms". It's still in development.`,
+        tags: [
+            {
+                name: "Next.js",
+                color: "black-text-gradient",
+            },
+            {
+                name: "Strapi",
+                color: "blue-text-gradient",
+            },
+        ],
+        image: kreics,
+        source_code_link: "https://github.com/GustavsIljuconoks/kreics.com",
+    },
 ];
 
 const roles = [
-  "Web Developer",
-  "Back-End Amateur",
-  "UI Design Enthusiast"
+    "Web Developer",
+    "Back-End Amateur",
+    "UI Design Enthusiast"
 ]
 
 export { technologies, projects, roles };
